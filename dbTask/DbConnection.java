ba4c822e9751d78beed5eca14af0a4b14b221519
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-07-17 19:47:19 +0530

    initial commit

--- dbTask/DbConnection.java
+++ dbTask/DbConnection.java
@@ -1,31 +1,23 @@
 package dbTask;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 public class DbConnection {
     public static Connection getConnection() {
-        Properties props = new Properties();
-        FileInputStream fis = null;
-        Connection con = null;
+        String url="jdbc:mysql://localhost/info";
+        String uname="root";
+        String pass="Password@1";
+        Connection con=null;
         try {
-            fis = new FileInputStream("db.properties");
-            props.load(fis);
-
             // load the Driver Class
-            Class.forName(props.getProperty("DB_DRIVER_CLASS"));
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
             // create the connection now
-            con = DriverManager.getConnection(props.getProperty("DB_URL"),
-                    props.getProperty("DB_USERNAME"),
-                    props.getProperty("DB_PASSWORD"));
-        } catch (IOException e) {
-            e.printStackTrace();
+            con = DriverManager.getConnection(url,uname,pass);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
         return con;
