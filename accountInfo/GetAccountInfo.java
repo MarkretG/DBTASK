2c2ab4fd7c0ebf12e6f7acf7fc2b858af8fb31d1
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-07-21 19:10:58 +0530

    initial commit

--- accountInfo/GetAccountInfo.java
+++ accountInfo/GetAccountInfo.java
@@ -5,14 +5,20 @@ import java.util.Map;
 public class GetAccountInfo {
     public static void getAccountInfo(int id)
     {
+        int flag=0;
         for (Map.Entry<Integer, HashMap<Long, Account>> information:ResultSetAreStoredInHashMap.info.entrySet()) {
             if (information.getKey() == id) {
                 HashMap<Long, Account> a = information.getValue();
                 for (Account account : a.values()) {
                     if (account.getCustomer_id() == id)
+                    {
+                        flag=1;
                         System.out.println(account.toString());
+                    }
                 }
             }
         }
+        if(flag==0)
+            System.out.println("entered info is does not exit in the table");
     }
 }
