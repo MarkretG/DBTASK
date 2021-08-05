102e5add30e5b7ad79587f6d94d246bb2cc2c229
MARKRET G <84516916+MarkretG@users.noreply.github.com>
2021-08-05 18:51:11 +0530

    Update DBManagementSystem.java

--- dbManagement/DBManagementSystem.java
+++ dbManagement/DBManagementSystem.java
@@ -7,48 +7,34 @@ public class DBManagementSystem {
         GeneralResource generalResource=GeneralResource.getInstance();
         DBUtil dbUtil=new DBUtil();
         System.out.println("welcome to db Management system");
-        System.out.println("1.Initially add rows in customer table and account table\n2.account info for given customer_id\n3.account info for given name\n4.Do you want to insert additional rows\n5.exit");
+        System.out.println("1.Add rows in customer table\n2.Add rows in account table\n3.Account info for given customer_id\n4.Account info for given name\n5.exit");
         while (true)
         {
             int choice=generalResource.getIntFromUser();
             switch (choice)
             {
                 case 1:
-                    dbUtil.addCustomersInfoInDb();
-                    dbUtil.addAccountsInfoInDb();
+                    dbUtil.storeCustomerInfoInCustomerHashMap(dbUtil.addCustomersInfoInDb());
+                    //dbUtil.addCustomersInfoInDb();
+
                     break;
                 case 2:
-                    System.out.println("enter customer id");
-                    long id = generalResource.getLongFromUser();
-                    HashMap<Long, Account> account=dbUtil.getAccountInfo(id);
-                    System.out.println(account.toString());
+                    dbUtil.storeAccountInfoInAccountsInfoHashMap(dbUtil.addAccountsInfoInDb());
+                    //dbUtil.addAccountsInfoInDb();
                     break;
                 case 3:
-                    System.out.println("enter name");
-                    String name = generalResource.getStringFromUser();
-                    long givenId=dbUtil.getId(name);
-                    HashMap<Long,Account> accounts=dbUtil.getAccountInfo(givenId);
-                    System.out.println(accounts.toString());
+                    System.out.println("enter customer id");
+                    long customerId = generalResource.getLongFromUser();
+                    HashMap<Long, Account> account=dbUtil.getAccountsInfoFromAccountsInfoHashMap(customerId);
+                    System.out.println(account.toString());
                     break;
                 case 4:
-                    System.out.println("1.Do you want insert row in customer table\n2.Do you want to insert row in accountTable\n3.exit");
-                    boolean end=true;
-                    while (end) {
-                        int ch = generalResource.getIntFromUser();
-                        switch (ch) {
-                            case 1:
-                                dbUtil.addCustomersInfoInDb();
-                                break;
-                            case 2:
-                                dbUtil.addAccountsInfoInDb();
-                                break;
-                            case 3:
-                                System.out.println("updated");
-                                end = false;
-                        }
-                    }
+                    System.out.println("enter name");
+                    String name = generalResource.getStringFromUser();
+                    long givenId=dbUtil.getCustomerIdFromCustomerHashMap(name);
+                    HashMap<Long,Account> accounts=dbUtil.getAccountsInfoFromAccountsInfoHashMap(givenId);
+                    System.out.println(accounts.toString());
                     break;
-
                 case 5:
                     generalResource.closeScanner();
                     dbUtil.closeConnection();
